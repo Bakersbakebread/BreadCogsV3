a73e7010b3cd4cbce40eebdb134608a830a8b1f8
Bread <seanthorne8@googlemail.com>
2019-05-24 15:33:35 +0100

    move modules to packages

---
+++ modmail/create/thread.py
@@ -0,0 +1,59 @@
+﻿import discord
+import asyncio
+from redbot.core import Config
+
+from redbot.core.utils.chat_formatting import humanize_list
+
+
+CONFIG = Config.get_conf(None, identifier=2807305259608965131, cog_name="ModMail")
+
+
+async def user_info_embed(user):
+    embed = discord.Embed(
+        title=f"{user.name} - {user.id}", description="""Information about user:"""
+    )
+    embed.set_thumbnail(url=user.avatar_url)
+
+    created_at = user.created_at.strftime("%b %d %Y")
+    # joined_at = user.joined_at.strftime("%b %d %Y")
+    async with CONFIG.user(user).info() as user_info:
+        history = len(user_info["archive"])
+    embed.add_field(name="Joined Discord", value=created_at)
+    embed.add_field(name="Amount of previous threads", value=history)
+    # embed.add_field(name="User's Roles",
+    #                value = humanize_list([x.name for x in user.roles]),
+    #                inline=False)
+
+    return embed
+
+async def format_channel_topic(user):
+    topic = f"**User** : {user.name}#{user.discriminator}\n"
+    topic += f"**ID** : {user.id}\n\n"
+    return topic
+
+async def new_modmail_thread(bot, guild, user):
+    thread_name: str = f"{NEW_THREAD_ICON}-{user.name}-{user.id}"
+
+    category = discord.utils.get(guild.categories, name=CATEGORY_NAME)
+
+    try:
+        new_thread = await guild.create_text_channel(
+            name=thread_name,
+            category=category,
+            topic=await format_channel_topic(user),
+            reason=f"New ModMail Thread for {user.name}",
+        )
+    except discord.errors.Forbidden as e:
+        print(f"[ModMail] {e} - could not create channel.")
+        return False
+
+    if not category:
+        # TODO: Move into DM owner?
+        category_error = "Could not assign channel to ModMail Category."
+        owner = await bot.get_user_info(bot.owner_id)
+        await owner.send(category_error)
+        await new_thread.send(category_error)
+
+    await new_thread.send(embed=await user_info_embed(user))
+
+    return new_thread
--- modmail/modmail.py
+++ modmail/modmail.py
@@ -1,9 +1,6 @@
 import discord
 from discord.ext import commands
 
-from . import create
-from .handle_messages import *
-
 import datetime
 import functools
 
@@ -12,228 +9,223 @@ from redbot.core.bot import Red
 
 from .utils import *
 
+from .create import thread, base
+from .messages import message_mods
+
 
 class Modmail(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-        self.config = Config.get_conf(self, identifier=2807305259608965131)
+	def __init__(self, bot):
+		self.bot = bot
+		self.config = Config.get_conf(self, identifier=2807305259608965131)
 
-        default_global = {"blocked": []}
-        self.config.register_global(**default_global)
+		default_global = {"blocked": []}
+		self.config.register_global(**default_global)
 
-        default_guild = {
-            "help_message_id": None,
-            "log_channel_id": None,
-            "category_id": None,
-        }
-        self.config.register_guild(**default_guild)
+		default_guild = {
+			"help_message_id": None,
+			"log_channel_id": None,
+			"category_id": None,
+		}
+		self.config.register_guild(**default_guild)
 
-        default_user = {
-            # TODO: Move blocked into here
-            "info": {
-                "counter": 0,
-                "last_messaged": None,
-                "guild_id": 0,
-                "thread_is_open": False,
-                "multi_guild_hold": False,
-                "thread_id": 0,
-                "current_thread": [],
-                "archive": [],
-            }
-        }
-        self.config.register_user(**default_user)
+		default_user = {
+			# TODO: Move blocked into here
+			"info": {
+				"counter": 0,
+				"last_messaged": None,
+				"guild_id": 0,
+				"thread_is_open": False,
+				"multi_guild_hold": False,
+				"thread_id": 0,
+				"current_thread": [],
+				"archive": [],
+			}
+		}
+		self.config.register_user(**default_user)
 
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
-        if isinstance(message.channel, discord.abc.PrivateChannel):
-            await message_mods(self.bot, message, self.config)
+	@commands.Cog.listener()
+	async def on_message(self, message):
+		if message.author == self.bot.user:
+			return
+		if isinstance(message.channel, discord.abc.PrivateChannel):
+			await message_mods.message(self.bot, message, self.config)
 
-    async def is_thread(self, guild, channel_to_check):
-        config_category_id = await self.config.guild(guild).category_id()
-        if channel_to_check.category is not None:
-            if channel_to_check.category.id != config_category_id:
-                return False
-        else:
-            return False
-        return True
+	async def is_thread(self, guild, channel_to_check):
+		config_category_id = await self.config.guild(guild).category_id()
+		if channel_to_check.category is not None:
+			if channel_to_check.category.id != config_category_id:
+				return False
+		else:
+			return False
+		return True
 
-    @commands.command()
-    async def create(self, ctx):
-        """Setup a new ModMail"""
-        loading = await ctx.send("⏳ Creating new ModMail...")
-        await create.new_modmail(ctx, self.config)
-        await loading.edit(content="👍 ModMail Created")
+	@commands.command()
+	async def create(self, ctx):
+		"""Setup a new ModMail"""
+		loading = await ctx.send("⏳ Creating new ModMail...")
+		await base.new_modmail(ctx, self.config)
+		await loading.edit(content="👍 ModMail Created")
 
-    @commands.command()
-    # async def aik(self, ctx):
-    async def aik(self, ctx, user: discord.Member):
-        await create.new_modmail_thread(self.bot, ctx.guild, user)
-        channel_check = ctx.guild.channels
-        for channel in channel_check:
-            if str(ctx.author.id) in channel.name:
-                await ctx.send(channel.name)
+	@commands.command()
+	async def clean(self, ctx):
+		for x in ctx.guild.channels:
+			if x.name != "test":
+				await x.delete()
 
-    @commands.command()
-    async def clean(self, ctx):
-        for x in ctx.guild.channels:
-            if x.name != "test":
-                await x.delete()
+	####################
+	# replying to user #
+	####################
 
-    ####################
-    # replying to user #
-    ####################
+	@commands.command()
+	async def reply(self, ctx, *, quick_message=None):
+		is_thread = await self.is_thread(ctx.guild, ctx.channel)
+		if not is_thread:
+			return await ctx.send("This is not a thread.")
 
-    @commands.command()
-    async def reply(self, ctx, *, quick_message=None):
-        is_thread = await self.is_thread(ctx.guild, ctx.channel)
-        if not is_thread:
-            return await ctx.send("This is not a thread.")
+		user_id = ctx.channel.topic.split()[5]
+		# user_id = 280730525960896513
+		user = await self.bot.get_user_info(user_id)
 
-        user_id = ctx.channel.topic.split()[5]
-        # user_id = 280730525960896513
-        user = await self.bot.get_user_info(user_id)
+		if quick_message:
+			message = quick_message
+		else:
+			service = await reply_service(ctx, user)
+			message = service[0]
 
-        if quick_message:
-            message = quick_message
-        else:
-            service = await reply_service(ctx, user)
-            message = service[0]
+		embed = discord.Embed(title="Message preview", description=message)
 
-        embed = discord.Embed(title="Message preview", description=message)
+		reply = await ctx.send(embed=embed)
 
-        reply = await ctx.send(embed=embed)
+		approved = await yes_or_no(
+			ctx, message=f"Would you like to send message to {user.name}?"
+		)
 
-        approved = await yes_or_no(
-            ctx, message=f"Would you like to send message to {user.name}?"
-        )
+		if approved:
+			await reply_to_user(ctx, self.config, message, user, is_anon=False)
+		else:
+			await ctx.send("Ok. Message not sent.", delete_after=30)
 
-        if approved:
-            await reply_to_user(ctx, self.config, message, user, is_anon=False)
-        else:
-            await ctx.send("Ok. Message not sent.", delete_after=30)
+		await reply.delete()
+		await ctx.message.delete()
+		if not quick_message:
+			await service[1].delete()
 
-        await reply.delete()
-        await ctx.message.delete()
-        if not quick_message:
-            await service[1].delete()
+	@commands.command()
+	async def replyanon(self, ctx, *, quick_message=None):
+		is_thread = await self.is_thread(ctx.guild, ctx.channel)
+		if not is_thread:
+			return await ctx.send("This is not a thread.")
 
-    @commands.command()
-    async def replyanon(self, ctx, *, quick_message=None):
-        is_thread = await self.is_thread(ctx.guild, ctx.channel)
-        if not is_thread:
-            return await ctx.send("This is not a thread.")
+		user_id = ctx.channel.topic.split()[5]
+		user_id = 280730525960896513
+		user = await self.bot.get_user_info(user_id)
+		if quick_message:
+			message = quick_message
+		else:
+			service = await reply_service(ctx, user)
+			message = service[0]
 
-        user_id = ctx.channel.topic.split()[5]
-        user_id = 280730525960896513
-        user = await self.bot.get_user_info(user_id)
-        if quick_message:
-            message = quick_message
-        else:
-            service = await reply_service(ctx, user)
-            message = service[0]
+		embed = discord.Embed(title="Message preview", description=message)
+		embed.set_footer(text=await get_label("info", "sending_anon"))
 
-        embed = discord.Embed(title="Message preview", description=message)
-        embed.set_footer(text=await get_label("info", "sending_anon"))
+		reply = await ctx.send(embed=embed)
 
-        reply = await ctx.send(embed=embed)
+		approved = await yes_or_no(
+			ctx, message=f"Would you like to send message to {user.name}?"
+		)
 
-        approved = await yes_or_no(
-            ctx, message=f"Would you like to send message to {user.name}?"
-        )
+		if approved:
+			await reply_to_user(ctx, self.config, message, user, is_anon=True)
+		else:
+			await ctx.send("Ok. Message not sent.", delete_after=30)
 
-        if approved:
-            await reply_to_user(ctx, self.config, message, user, is_anon=True)
-        else:
-            await ctx.send("Ok. Message not sent.", delete_after=30)
+		await reply.delete()
+		await ctx.message.delete()
+		if not quick_message:
+			await service[1].delete()
 
-        await reply.delete()
-        await ctx.message.delete()
-        if not quick_message:
-            await service[1].delete()
+	#####################
+	# closing thread    #
+	#####################
+	@commands.command()
+	async def close(self, ctx, time_delay=None):
+		is_thread = await self.is_thread(ctx.guild, ctx.channel)
+		if not is_thread:
+			return await ctx.send("This is not a thread.")
 
-    #####################
-    # closing thread    #
-    #####################
-    @commands.command()
-    async def close(self, ctx, time_delay=None):
-        is_thread = await self.is_thread(ctx.guild, ctx.channel)
-        if not is_thread:
-            return await ctx.send("This is not a thread.")
+		approved = await yes_or_no(
+			ctx, message="Are you sure you want to close this thread?"
+		)
+		if not approved:
+			await ctx.message.delete()
+			return await ctx.send("Okay. Thread will remain open.", delete_after=30)
 
-        approved = await yes_or_no(
-            ctx, message="Are you sure you want to close this thread?"
-        )
-        if not approved:
-            await ctx.message.delete()
-            return await ctx.send("Okay. Thread will remain open.", delete_after=30)
+		loading = await ctx.send(":hourglass: Closing ModMail Thread")
 
-        loading = await ctx.send(":hourglass: Closing ModMail Thread")
+		user_id = ctx.channel.topic.split()[5]
+		user = await self.bot.get_user_info(user_id)
 
-        user_id = ctx.channel.topic.split()[5]
-        user = await self.bot.get_user_info(user_id)
+		async with self.config.user(user).info() as user_info:
+			current_thread = {user_info["thread_id"]: user_info["current_thread"]}
 
-        async with self.config.user(user).info() as user_info:
-            current_thread = {user_info["thread_id"]: user_info["current_thread"]}
+			user_info.update(
+				{"thread_is_open": False, "thread_id": 0, "current_thread": []}
+			)
 
-            user_info.update(
-                {"thread_is_open": False, "thread_id": 0, "current_thread": []}
-            )
+			user_info["archive"].append(current_thread)
 
-            user_info["archive"].append(current_thread)
+		embed = discord.Embed(
+			title=f" ",
+			description=f"ModMail Thread closed by {ctx.author.name}",
+			color=discord.Color.red(),
+		)
+		await loading.edit(embed=embed, content=f" ")
+		approved = await yes_or_no(ctx, message="Would you like to delete the channel?")
+		if approved:
+			await ctx.send("Delete")
+		else:
+			await ctx.send("dont")
 
-        embed = discord.Embed(
-            title=f" ",
-            description=f"ModMail Thread closed by {ctx.author.name}",
-            color=discord.Color.red(),
-        )
-        await loading.edit(embed=embed, content=f" ")
-        approved = await yes_or_no(ctx, message="Would you like to delete the channel?")
-        if approved:
-            await ctx.send("Delete")
-        else:
-            await ctx.send("dont")
+	#####################
+	# blocking users   #
+	####################
+	@staticmethod
+	async def is_user_blocked(user, config):
+		"""Find out if user is blocked"""
+		async with config.blocked() as blocked:
+			if user.id in blocked:
+				return True
+			else:
+				return False
 
-    #####################
-    # blocking users   #
-    ####################
-    @staticmethod
-    async def is_user_blocked(user, config):
-        """Find out if user is blocked"""
-        async with config.blocked() as blocked:
-            if user.id in blocked:
-                return True
-            else:
-                return False
+	async def toggle_blocked(self, user):
+		async with self.config.blocked() as blocked:
+			if user.id in blocked:
+				blocked.remove(user.id)
+			else:
+				blocked.append(user.id)
 
-    async def toggle_blocked(self, user):
-        async with self.config.blocked() as blocked:
-            if user.id in blocked:
-                blocked.remove(user.id)
-            else:
-                blocked.append(user.id)
+	@commands.command()
+	async def block(self, ctx, user: discord.Member):
+		"""Block a user from message mod's."""
+		if user.id == ctx.me.id:
+			return await ctx.send(":thinking: It's not a good idea to block me.")
 
-    @commands.command()
-    async def block(self, ctx, user: discord.Member):
-        """Block a user from message mod's."""
-        if user.id == ctx.me.id:
-            return await ctx.send(":thinking: It's not a good idea to block me.")
+		user_blocked = await self.is_user_blocked(user, self.config)
+		if not user_blocked:
+			await self.toggle_blocked(user)
+			await ctx.send(f":shield: :mute: {user.name} has been blocked.")
+		else:
+			await ctx.send(f":no_entry: :mute: {user.name} is already blocked.")
 
-        user_blocked = await self.is_user_blocked(user, self.config)
-        if not user_blocked:
-            await self.toggle_blocked(user)
-            await ctx.send(f":shield: :mute: {user.name} has been blocked.")
-        else:
-            await ctx.send(f":no_entry: :mute: {user.name} is already blocked.")
-
-    @commands.command()
-    async def unblock(self, ctx, user: discord.Member):
-        """Block a user from message mod's."""
-        if user.id == ctx.me.id:
-            await ctx.send(":thinking:")
-        blocked = await self.is_user_blocked(user, self.config)
-        if blocked:
-            await self.toggle_blocked(user)
-            await ctx.send(f":loud_sound: {user.name} has been unblocked.")
-        else:
-            await ctx.send(f":no_entry: {user.name} is not blocked.")
+	@commands.command()
+	async def unblock(self, ctx, user: discord.Member):
+		"""Block a user from message mod's."""
+		if user.id == ctx.me.id:
+			await ctx.send(":thinking:")
+		blocked = await self.is_user_blocked(user, self.config)
+		if blocked:
+			await self.toggle_blocked(user)
+			await ctx.send(f":loud_sound: {user.name} has been unblocked.")
+		else:
+			await ctx.send(f":no_entry: {user.name} is not blocked.")
