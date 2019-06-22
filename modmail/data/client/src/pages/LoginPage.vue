dcac3fe07a1b4ed71cfe5f6b4ed2d776765b1ddd
Unknown <bakersbakebread@github.com>
2019-06-22 11:48:27 +0100

    add Login-Page

---
+++ modmail/data/client/src/pages/LoginPage.vue
@@ -0,0 +1,17 @@
+<template>
+<div class="container">
+  test
+  <a
+  href="/api/discord/login"
+  class="btn btn-block">Login</a>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'Login'
+}
+</script>
+
+<style scoped>
+</style>
