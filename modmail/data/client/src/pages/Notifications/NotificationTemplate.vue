b4882bf1f6b430824d4e87b7c14eac53e51a65c6
Bread <bakersbakebread@github.com>
2019-07-21 02:13:35 +0100

    delete modmail from master, keeps dev

--- modmail/data/client/src/pages/Notifications/NotificationTemplate.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <span>Welcome to <b>ModMail!</b> - this site is still under development and I welcome any support.</span>
-</template>
-
-<script>
-  export default {
-    name: 'notification-template'
-  }
-</script>
